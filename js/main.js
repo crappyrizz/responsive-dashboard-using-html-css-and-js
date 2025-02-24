66320ef9dda3339714ea5f37158bfd165d4c57a2
crappyrizz <155568733+crappyrizz@users.noreply.github.com>
2025-02-24 16:46:37 +0300

    working on the menu, toggle and search bar

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// add hovered class to selected list item
+let list = document.querySelectorAll(".navigation li");
+
+function activeLink() {
+    list.forEach((item) => {
+        item.classList.remove("hovered");
+    });
+    this.classList.add("hovered");
+}
+
+list.forEach(item => item.addEventListener("mouseover", activeLink))
+
+//menu toggle
+let toggle = document.querySelector(".toggle")
+let navigation - document.querySelector(".navigation")
